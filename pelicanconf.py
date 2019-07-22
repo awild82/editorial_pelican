dccfddc17a4761465e0608f7b74ff2b1696d26a3
Andrew Wildman <apw4@uw.edu>
2019-07-21 22:32:54 -0700

    Enable pagination. Clean pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,37 +1,54 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
+
+#
+# Site Metadata
+#
 
 AUTHOR = 'A.N. Other'
 SITENAME = 'Editorial'
 SITESUBTITLE = 'A pelican theme'
 SITEURL = ''
 
+TIMEZONE = 'America/Los_Angeles'
+DEFAULT_LANG = 'en'
+
+#
+# Paths and output locations
+#
+
 PATH = 'content'
 STATIC_PATHS = ['images', 'extra']
 EXTRA_PATH_METADATA = {
     'extra/favicon.ico': {'path': 'favicon.ico'}
 }
+DIRECT_TEMPLATES = ['search', 'index', 'archives', 'authors', 'categories',
+                    'tags', '404']
+PAGINATED_DIRECT_TEMPLATES = ['archives']
+
+#
+# Plugins
+#
 
 PLUGIN_PATHS = ['plugins']
 PLUGINS = ['render_math', 'tipue_search']
 
+#
+# Theme
+#
+
 THEME = 'theme'
-DIRECT_TEMPLATES = ['search', 'index', 'archives', 'authors', 'categories',
-                    'tags', '404']
 
-TIMEZONE = 'America/Los_Angeles'
 
-DEFAULT_LANG = 'en'
+#
+# Custom theme elements
+#
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+# Sidebar menu links
+#   Entries can be one of:
+#     direct-link: (<display text>, <link relative to root>)
+#     submenu: (<display text>, (<direct-link>), [<direct-link>]...))
+#   See README.md for more details
 
-# Sidebar links
 MENU_LINKS = (('Homepage', ''),
               ('Generic', 'pages/generic'),
               ('Theme elements', 'elements'),
@@ -42,23 +59,21 @@ MENU_LINKS = (('Homepage', ''),
                   ('Tags', 'tags'))),
               ('You can add entries in your config file', '#'),)
 
+# Contacts at the end of the sidebar
+#   Entries are:
+#     (<display text>, <link (or None)>, <icon class name>)
+
 CONTACTS = (('information@untitled.tld', '#', 'solid fa-envelope'),
             ('(000) 000-0000', None, 'solid fa-phone'),
             ('1234 Somewhere Road #8254 <br> Nashville, TN 00000-0000',
              None, 'solid fa-home'))
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# Social icons in header bar
+#   Entries are:
+#     (<icon class name>, <link>)
 
-# Social widget
 SOCIAL = (('brands fa-github', '#'),
           ('brands fa-gitlab', '#'),
           ('fa-envelope', '#'))
 
-DEFAULT_PAGINATION = True
-
-# Uncomment following line if you want document-relative URLs when developing
-# RELATIVE_URLS = True
+DEFAULT_PAGINATION = 6
