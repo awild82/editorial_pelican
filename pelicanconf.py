6582ce1d834c35a08c967d630139ae83115160b7
Andrew Wildman <apw4@uw.edu>
2020-10-26 10:54:07 -0700

    Add share_post buttons and tags to article beginning

--- pelicanconf.py
+++ pelicanconf.py
@@ -30,7 +30,7 @@ PAGINATED_TEMPLATES = {'archives': None, 'tag': None, 'category': None, 'author'
 #
 
 PLUGIN_PATHS = ['plugins']
-PLUGINS = ['render_math', 'tipue_search']
+PLUGINS = ['render_math', 'tipue_search', 'share_post']
 
 #
 # Theme
@@ -74,5 +74,18 @@ CONTACTS = (('information@untitled.tld', '#', 'solid fa-envelope'),
 SOCIAL = (('brands fa-github', 'https://github.com/awild82/editorial_pelican'),
           ('brands fa-gitlab', 'https://gitlab.com/awild82/editorial_pelican'),
           ('fa-envelope', '#'))
+
+# Share post options
+#  Entries are:
+#    (<name>, <share_post_link>, <show>)
+#  share_post_link can be one of:
+#    facebook, twitter, email, diaspora, linkedin, hacker-news, reddit
+SHAREPOST = (('Facebook', 'facebook', True),
+             ('Email', 'email', True),
+             ('Twitter', 'twitter', True),
+             ('Diaspora*', 'diaspora', True),
+             ('LinkedIn', 'linkedin', True),
+             ('Hacker News', 'hacker-news', True),
+             ('Reddit', 'reddit', True))
 
 DEFAULT_PAGINATION = 6
